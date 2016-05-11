8f5113db10d0a239eb3dac65613e40dcebecec35
‘Aarman’ <aaron.beerman@gmail.com>
2016-05-11 00:36:43 -0700

    css changes to font icons

--- blogobjects.js
+++ blogobjects.js
@@ -13,6 +13,14 @@ var myProjects = [
     completionDate: '2016-03-20',
     body:        '<p>Class project created to help researchers determine which products are preferred. </p> <img src="nw.png" class="">'
   },
+  {
+    title:       'PDX ',
+    role:        'Co-developer',
+    projectUrl:   'http://pdx-neighbors.github.io/pdx-neighbors/',
+    completionDate: '2016-04-30',
+    body:        '<p>PDX Neighbors was a group, class project created to help new Portland area residents determine which neighborhood best suits them.</p> <img src="nw.png" class="">'
+  },
+
 ];
 
 console.log(myProjects);
